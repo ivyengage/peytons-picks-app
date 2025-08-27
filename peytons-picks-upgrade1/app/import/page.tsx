091589fca446bf4ec2d0a745099e4b315f8725ec
ivyengage <dowens@ivyengage.com>
2025-08-27 12:30:13 -0500

    Delete peytons-picks-upgrade1 directory

--- peytons-picks-upgrade1/app/import/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ImportPage() {
-  return (
-    <main>
-      <h2>Import Screenshots</h2>
-      <p>Multi-screenshot OCR importer will land next. For now, use <a href="/upload">Upload CSV</a>.</p>
-    </main>
-  );
-}
--- peytons-picks-upgrade1/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from 'pg';
-export async function getClient() {
-  const url = process.env.DATABASE_URL;
-  if (!url) throw new Error('DATABASE_URL not set');
-  const client = new Client({ connectionString: url, ssl: { rejectUnauthorized: false } });
-  await client.connect();
-  return client;
-}
